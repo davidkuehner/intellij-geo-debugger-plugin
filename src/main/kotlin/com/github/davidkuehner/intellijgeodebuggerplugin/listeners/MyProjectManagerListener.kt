66744cf56e3cc69546c13f9b6ab85d12d194c3b0
GitHub Action <action@github.com>
2021-07-30 10:53:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/davidkuehner/intellijgeodebuggerplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/davidkuehner/intellijgeodebuggerplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.davidkuehner.intellijgeodebuggerplugin.listeners
 
+import com.github.davidkuehner.intellijgeodebuggerplugin.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/davidkuehner/intellijgeodebuggerplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.davidkuehner.intellijgeodebuggerplugin.services
+
+import com.github.davidkuehner.intellijgeodebuggerplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
